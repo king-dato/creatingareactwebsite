20db3c6b8909f711436356d075cc870506451039
king-dato <king_dato@yahoo.com>
2025-01-31 00:47:01 -0800

    Update_myapp

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import messenger from "../components/messenger";
+import { messenger } from "./components/messenger";
 
 function App() {
   return (

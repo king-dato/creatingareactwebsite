98e2fed8320db9b64ae2ccb650ae9be00ff901dc
king-dato <king_dato@yahoo.com>
2025-01-30 21:53:55 -0800

    Update

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import messenger from "../components/messenger";
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hi welcome to my webiste
         </a>
       </header>
     </div>

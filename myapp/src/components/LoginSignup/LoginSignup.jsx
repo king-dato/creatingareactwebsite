9850fcd70d0c8dc03a00c4eb4f4f7e53c6e78a77
king-dato <king_dato@yahoo.com>
2025-02-13 06:25:56 -0800

    Update for axios and hook for the password

--- myapp/src/components/LoginSignup/LoginSignup.jsx
+++ myapp/src/components/LoginSignup/LoginSignup.jsx
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
+import { useRef, useState, useEffect } from "react";
+import {
+  faCheck,
+  faTimes,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+const PWD_regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 import user_icon from "../Assets/person.png";
 import email_icon from "../Assets/email.png";
 import password_icon from "../Assets/password.png";

57329de151470fbd7b889e3f787ad0f71571e7ed
king-dato <king_dato@yahoo.com>
2025-03-12 15:31:07 -0700

    Update Home

--- myapp/src/components/Home.js
+++ myapp/src/components/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { StyledEngineProvider } from "@mui/material/styles";
 import Appbar from "./Appbar";
+import ImgList from "./imagelist";
 
 function Home() {
   return (
     <>
       <StyledEngineProvider injectFirst>
         <Appbar />
+        <ImgList />
       </StyledEngineProvider>
     </>
   );
